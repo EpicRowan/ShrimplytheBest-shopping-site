96c061ce762dc2cff44e20d193a7efb549099de4
Rowan Shepherd <shepherd.rowan.k@gmail.com>
2020-05-23 21:12:51 -0700

    Created function to read shrimp data from file

--- shrimp.py
+++ shrimp.py
@@ -19,3 +19,37 @@ class Shrimp(object):
 		self.name = name
 		self.price = price
 		self.image_url  = image_url
+
+ 	def __repr__(self):
+        """Convenience method to show information about shrmp in console."""
+
+        return "<Shrimp: {}, {}, {}>".format(self.shrimp_id, self.name, self.price_str())
+
+
+def read_shrimp_from_file(filepath):
+    """Read shrimp type data and populate dictionary.
+
+    Dictionary will be {id: Shrimp object}
+    """
+
+    shrimp_types = {}
+
+    with open(filepath) as file:
+        for line in file:
+			   (shrimp_id,
+				name,
+				price,
+				image_url,
+				) = line.strip().split("|")
+    
+            price = float(price)
+    
+    
+            shrimp_types[shrimp_id] = Shrimp(shrimp_id,
+											 name,
+											 price,
+											 image_url,
+											 )
+
+    return shrimp_types
+
